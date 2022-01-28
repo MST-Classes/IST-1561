c80c03ff3273a48c0d58b2771c18f237c3ed9ba4
Dark <nehall.neh@gmail.com>
2022-01-28 10:59:53 -0600

    code cleanup

--- src/main/java/me/nhall/ist/IST.java
+++ src/main/java/me/nhall/ist/IST.java
@@ -1,15 +1,12 @@
 package me.nhall.ist;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class IST {
 
     public static void main(String[] args) {
-        char[] chars = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o','r','l','d'};
+        char[] chars = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
         StringBuilder printMe = new StringBuilder();
 
-        for(char c : chars) {
+        for (char c : chars) {
             printMe.append(c);
         }
         System.out.println(printMe);
