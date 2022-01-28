b1082bd20f0f82a6a144ab02e4492f6fe571b667
Dark <nehall.neh@gmail.com>
2022-01-28 11:05:03 -0600

    lab-1 complete

--- Lab-1/src/main/java/me/nhall/lab/Lab1.java
+++ Lab-1/src/main/java/me/nhall/lab/Lab1.java
@@ -3,6 +3,13 @@ package me.nhall.lab;
 public class Lab1 {
 
     public static void main(String[] args) {
+        char[] chars = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+        StringBuilder printMe = new StringBuilder();
 
+        for (char c : chars) {
+            printMe.append(c);
+        }
+        System.out.println(printMe);
+        System.out.println("This is Nolan's first java program!");
     }
 }
