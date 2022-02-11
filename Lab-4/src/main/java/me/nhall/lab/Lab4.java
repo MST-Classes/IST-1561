770ce4d784fc78a84f5c8197aea9d8e936acf255
Dark <nehall.neh@gmail.com>
2022-02-11 11:04:54 -0600

    add lab4 module

---
+++ Lab-4/src/main/java/me/nhall/lab/Lab4.java
@@ -0,0 +1,14 @@
+package me.nhall.lab;
+
+public class Lab4 {
+
+    public static void main(String[] args) {
+        System.out.println(6 / 7 + 3.5 + 4.5);
+        System.out.println(3 + 4 * 2 > 2 * 9);
+        System.out.println(3 * 4 + 5 * 6 + 7 * -2);
+        System.out.println(1.5 * 2.0 + (5.5 / 2) + 5 / 4);
+        System.out.println(23 % 5 + 31 / 4 % 3 - 17 % (16 % 10) );
+        System.out.println(345 / 10 / 3 * 55 / 5 / 6 + 10 / (5 / 2.0));
+
+    }
+}
