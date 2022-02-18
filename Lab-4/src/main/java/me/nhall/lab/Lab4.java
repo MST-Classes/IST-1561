b47d2adc5744c69ef9e6a19e1ac890ede211e951
Nolan <nehall.neh@gmail.com>
2022-02-18 11:10:06 -0600

    Lab4 complete

--- Lab-4/src/main/java/me/nhall/lab/Lab4.java
+++ Lab-4/src/main/java/me/nhall/lab/Lab4.java
@@ -1,14 +1,26 @@
 package me.nhall.lab;
 
+import java.util.Scanner;
+
 public class Lab4 {
 
     public static void main(String[] args) {
-        System.out.println(6 / 7 + 3.5 + 4.5);
-        System.out.println(3 + 4 * 2 > 2 * 9);
-        System.out.println(3 * 4 + 5 * 6 + 7 * -2);
-        System.out.println(1.5 * 2.0 + (5.5 / 2) + 5 / 4);
-        System.out.println(23 % 5 + 31 / 4 % 3 - 17 % (16 % 10) );
-        System.out.println(345 / 10 / 3 * 55 / 5 / 6 + 10 / (5 / 2.0));
+        Scanner sc = new Scanner(System.in);
+        int smallNum;
 
+        System.out.println("Please enter 3 integers:");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        if (num1 <= num2 && num1 <= num3) {
+            smallNum = num1;
+        } else if (num2 <= num1 && num2 <= num3) {
+            smallNum = num2;
+        } else {
+            smallNum = num3;
+        }
+
+        System.out.println("The smallest number is " + smallNum);
     }
 }
