92cc3ffddee68ef35b0f5306878dab6ff8c88a6e
Dark <nehall.neh@gmail.com>
2022-03-04 11:07:50 -0600

    finish lab6

--- Lab-6/src/main/java/me/nhall/lab/Lab6.java
+++ Lab-6/src/main/java/me/nhall/lab/Lab6.java
@@ -1,8 +1,24 @@
 package me.nhall.lab;
 
+import java.util.Scanner;
+
 public class Lab6 {
 
     public static void main(String[] args) {
+
+        double mass, force, angle;
+        double g = 9.8;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the mass of the cart: ");
+        mass = sc.nextDouble();
+
+        System.out.print("Enter force to push the cart: ");
+        force = sc.nextDouble();
+
+        angle = Math.toDegrees(Math.asin(force / (mass * g)));
+        angle = (double) Math.round(angle * 100) / 100;
+        System.out.println("The angle of the ramp is: " + angle + " degrees");
 
     }
 
