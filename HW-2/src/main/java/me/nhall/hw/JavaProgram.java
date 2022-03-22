6e0587bde99dd125b9eb64515facd746f32d3ba3
Dark <nehall.neh@gmail.com>
2022-03-21 19:00:23 -0500

    add lockerpuzzle to hw2

--- HW-2/src/main/java/me/nhall/hw/JavaProgram.java
+++ HW-2/src/main/java/me/nhall/hw/JavaProgram.java
@@ -8,24 +8,20 @@ public class JavaProgram {
         while (x < 5) {
 
             //#1
-            // y= x - y;
-            // 00 11 21 32 42 ---- E
+            //y = x - y;
 
             //#2
-            // y = y + x;
-            // 00 11 23 36 410 ---- G
+            //y = y + x;
 
             //#3
             //y = y + 2;
             //if (y > 4) {
             //y = y - 1;
             //}
-            // 02 14 25 36 47 ---- H
 
             //#4
-            // = x + 1;
+            //x = x + 1;
             //y = y + x;
-            // 11 34 59 ---- B
 
             //#5
             //if (y < 5) {
@@ -35,7 +31,6 @@ public class JavaProgram {
             //    }
             // }
             //y = y + 2;
-            //02 14 36 48 ---- D
 
             System.out.print(x + "" + y + " ");
             x = x + 1;
---
+++ HW-2/src/main/java/me/nhall/hw/LockerPuzzle.java
@@ -0,0 +1,28 @@
+package me.nhall.hw;
+
+public class LockerPuzzle {
+
+    public static void main(String[] args) {
+
+        boolean[] locker = new boolean[101];
+
+        for (int i = 1; i < locker.length; i++) {
+            locker[i] = false;
+        }
+        for (int i = 1; i < locker.length; i++) {
+            locker[i] = true;
+        }
+        for (int S = 2; S < locker.length; S++) {
+            for (int k = S; k < locker.length; k = k + S) {
+                locker[k] = !locker[k];
+
+            }
+        }
+        for (int S = 1; S < locker.length; S++) {
+            if (locker[S]) System.out.println("Locker " + S + " Open");
+        }
+    }
+
+
+}
+
